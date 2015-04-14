bf7136d1aab28a7c965d48eb9df2ab9a0e128a65
adamyoung600 <adamyoung600@gmail.com>
2015-04-14 12:38:41 -0700

    Basic Home Page and navigatability to the HUD Settings Page

--- WrxControlCenter/MainPage.xaml.cs
+++ WrxControlCenter/MainPage.xaml.cs
@@ -22,9 +22,40 @@ namespace WrxControlCenter
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        public List<MainPageOption> applicationOptions = new List<MainPageOption>();
+
         public MainPage()
         {
             this.InitializeComponent();
+            applicationOptions.Add(new MainPageOption("Data Logging", "/Assets/DynoPlot2.jpg"));
+            applicationOptions.Add(new MainPageOption("Engine Monitoring", "/Assets/GhostViewEngine.jpg"));
+            applicationOptions.Add(new MainPageOption("HUD Settings", "/Assets/Gears.jpg"));
+            optionsGridView.DataContext = applicationOptions;
         }
+        
+        public void OptionClicked(object sender, ItemClickEventArgs e)
+        {
+            MainPageOption selection = e.ClickedItem as MainPageOption;
+            String title = selection.myTitle;
+            if (title.Equals("HUD Settings"))
+            {
+                this.Frame.Navigate(typeof(HUDSettingsPage));
+            }
+        }
+   
+    }
+
+    public class MainPageOption
+    {
+
+        public MainPageOption(String inTitle, String inImage)
+        {
+            myTitle = inTitle;
+            myImage = inImage;
+        }
+
+        public String myTitle { get; set; }
+        public String myImage { get; set; }
     }
 }
